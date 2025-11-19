09f73d3527c49e58400b9abeeb68002d17d5736f
Kryu <kryu_maro_veracruz@dlsl.edu.ph>
2025-11-19 12:17:14 +0800

    put main.js beside main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Login from "./pages/Login.jsx";
 import "../styles.css";
+import("/src/main.js");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
